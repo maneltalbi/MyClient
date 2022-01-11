64d31baa69861df17648e33b098c6a286f95d47c
manel talbi <talbimanel20@gmail.com>
2022-01-11 13:49:04 +0100

    use created library

---
+++ MyClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using MyCodeLibrary;
+using System;
+
+
+
+
+namespace MyClient
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Scrape myScrape = new Scrape();
+            string value = myScrape.ScrapeWebpage("http://msdn.microsoft.com");
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
